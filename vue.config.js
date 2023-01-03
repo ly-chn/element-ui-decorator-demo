d54ccbd11a9bb11cb8c8bf22226f0959486602d8
xy <3293232930@qq.com>
2023-01-03 11:17:33 +0800

    update public path

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: '/element-ui-decorator-demo',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
